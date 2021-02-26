d944ede01c36538f23615049b0f4ffecb1c44ebd
Mathéo Grail <grail.matheo@protonmail.com>
2021-02-26 09:33:12 +0100

    Ajout des headers pour éviter les erreurs CORS

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-// TO DO : header....
+// Cross Origin Resource Sharing.
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
 // Importation de la configuration.
 require('dotenv').config()
