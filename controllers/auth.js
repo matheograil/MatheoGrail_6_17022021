1c516bde21dbccde383a7f0f7874c72f86a5bb70
Mathéo Grail <grail.matheo@protonmail.com>
2021-03-07 20:25:24 +0100

    Revert "Amélioration globale du code (1)"
    This reverts commit 49ec1288a450830846224603ca8b839b897f6932.

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,32 +10,34 @@ const sanitize = require('mongo-sanitize');
 exports.register = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-    const UserValidator = new Validator({ email: email, password: password }, {
+    const UserValidator = new Validator({
         email: 'required|email|maxLength:50',
         password: 'required|string|lengthBetween:10,100'
     });
     // Vérification des données reçues.
     UserValidator.check().then(matched => {
-        if (!matched) {
-            return res.status(400).json({ error: 'Les identifiants sont incorrects.' });
-        }
-        // L'utilisateur existe-t-il ?
-        User.findOne({ email: sanitize(email) }).then(user => {
-            if (user) {
-                return res.status(400).json({ error: 'Cette adresse électronique est déjà utilisée.' });
-            }
-            // Chiffrement du mot de passe.
-            bcrypt.hash(password, 10).then(hash => {
-                const user = new User({
-                    email: sanitize(email),
-                    password: sanitize(hash)
-                });
-                // Enregistrement dans la base de données.
-                user.save()
-                .then(() => res.status(200).json({ message: "L'utilisateur a été enregistré." }))
-                .catch(() => res.status(500).json({ error: "Une erreur s'est produite." }));
+        if (matched) {
+            // L'utilisateur existe-t-il ?
+            User.findOne({ email: sanitize(email) }).then(user => {
+                if (!user) {
+                    // Chiffrement du mot de passe.
+                    bcrypt.hash(password, 10).then(hash => {
+                        const user = new User({
+                            email: sanitize(email),
+                            password: sanitize(hash)
+                        });
+                        // Enregistrement dans la base de données.
+                        user.save()
+                            .then(() => res.status(200).json({ message: "L'utilisateur a été enregistré." }))
+                            .catch(() => res.status(500).json({ error: "Une erreur s'est produite." }));
+                    }).catch(() => res.status(500).json({ error: "Une erreur s'est produite." }));
+                } else {
+                    res.status(400).json({ error: 'Cette adresse électronique est déjà utilisée.' });
+                }
             }).catch(() => res.status(500).json({ error: "Une erreur s'est produite." }));
-        }).catch(() => res.status(500).json({ error: "Une erreur s'est produite." }));
+        } else {
+            res.status(400).json({ error: 'Les identifiants sont incorrects.' });
+        }
     }).catch(() => res.status(500).json({ error: "Une erreur s'est produite." }));
 };
 
@@ -43,35 +45,38 @@ exports.register = (req, res, next) => {
 exports.login = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-    const UserValidator = new Validator({ email: email, password: password }, {
+    const UserValidator = new Validator({
         email: 'required|email|maxLength:50',
         password: 'required|string|lengthBetween:10,100'
     });
     // Vérification des données reçues.
     UserValidator.check().then(matched => {
-        if (!matched) {
-            return res.status(400).json({ error: 'Les identifiants sont incorrects.' });
-        }
-        // L'utilisateur existe-t-il ?
-        User.findOne({ email: sanitize(email) }).then(user => {
-            if (!user) {
-                return res.status(400).json({ error: 'Les identifiants sont incorrects.' });
-            }
-            // Le mot de passe correspond-t-il ?
-            bcrypt.compare(password, user.password).then(valid => {
-                if (!valid) {
-                    return res.status(400).json({ error: 'Les identifiants sont incorrects.' });
+        if (matched) {
+            // L'utilisateur existe-t-il ?
+            User.findOne({ email: sanitize(email) }).then(user => {
+                if (!user) {
+                    res.status(400).json({ error: 'Les identifiants sont incorrects.' });
+                } else {
+                    // Le mot de passe correspond-t-il ?
+                    bcrypt.compare(password, user.password).then(valid => {
+                          if (!valid) {
+                            res.status(400).json({ error: 'Les identifiants sont incorrects.' });
+                          } else {
+                            // Enregistrement du jeton d'accès.
+                            res.status(200).json({
+                                userId: user._id,
+                                token: jsonwebtoken.sign(
+                                    { userId: user._id },
+                                    process.env.JWT_TOKEN,
+                                    { expiresIn: '12h' }
+                                )
+                            });
+                        }
+                    }).catch(() => res.status(500).json({ error: "Une erreur s'est produite." }));
                 }
-                // Enregistrement du jeton d'accès.
-                res.status(200).json({
-                    userId: user._id,
-                    token: jsonwebtoken.sign(
-                        { userId: user._id },
-                        process.env.JWT_TOKEN,
-                        { expiresIn: '12h' }
-                    )
-                });
             }).catch(() => res.status(500).json({ error: "Une erreur s'est produite." }));
-        }).catch(() => res.status(500).json({ error: "Une erreur s'est produite." }));
+        } else {
+            res.status(400).json({ error: 'Les identifiants sont incorrects.' });
+        }
     }).catch(() => res.status(500).json({ error: "Une erreur s'est produite." }));
 };
--- middlewares/sauces.js
+++ middlewares/sauces.js
@@ -5,28 +5,32 @@ const fs = require('fs');
 const sanitize = require('mongo-sanitize');
 
 // Fonction permettant de supprimer une image.
-async function deleteImage(filename) {
-    fs.unlink(`./images/${filename}`, error => {
-        if (error) {
-            throw new Error(error);
-        } else {
-            return('Success');
-        }
+function deleteImage(filename) {
+    return new Promise(function(resolve, reject) {
+        fs.unlink(`./images/${filename}`, err => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve('Success');
+            }
+        });
     });
 };
 module.exports.deleteImage = deleteImage;
 
 // Fonction permettant de savoir si l'utilisateur a aimé ou non une sauce.
 async function doesUserHaveReview(array, userId) {
-    try {
-        if (array.indexOf(userId) != -1) {
-            return({ result: true, iterations: array.indexOf(userId), totalLikesOrDislikes: array.length });
-        } else {
-            return({ result: false, totalLikesOrDislikes: array.length })
+    return new Promise(function(resolve, reject) {
+        try {
+            if (array.indexOf(userId) != -1) {
+                resolve({result: true, iterations: array.indexOf(userId), totalLikesOrDislikes: array.length});
+            } else {
+                resolve({result: false, totalLikesOrDislikes: array.length})
+            }
+        } catch {
+            reject('Error');
         }
-    } catch(error) {
-        throw new Error(error);
-    }
+    });
 };
 module.exports.doesUserHaveReview = doesUserHaveReview;
 
@@ -40,8 +44,8 @@ function review(array, userId, iterations, action, totalLikesOrDislikes) {
             totalLikesOrDislikes--;
             array.splice(iterations, 1);
         }
-        return({ array: array, totalLikesOrDislikes: totalLikesOrDislikes });
-    } catch(error) {
+        return({array: array, totalLikesOrDislikes: totalLikesOrDislikes});
+    } catch {
         return('Error');
     }
 };
@@ -49,16 +53,18 @@ module.exports.review = review;
 
 // Fonction permettant de mettre à jour la sauce avec l'avis de l'utilisateur.
 async function putReview(usersLiked, usersDisliked, sauceId, totalLikesOrDislikes) {
-    if (usersLiked) {
-        Sauce.where('_id', sanitize(sauceId)).updateOne({ usersLiked: sanitize(usersLiked), likes: sanitize(totalLikesOrDislikes) }).then(() => {
-            return('Success');
-        }).catch(err => reject(err));
-    } else if (usersDisliked) {
-        Sauce.where('_id', sanitize(sauceId)).updateOne({ usersDisliked: sanitize(usersDisliked), dislikes: sanitize(totalLikesOrDislikes) }).then(() => {
-            return('Success');
-        }).catch(err => reject(err));
-    } else {
-        throw new Error('Error');
-    }
+    return new Promise(function(resolve, reject) {
+        if (usersLiked) {
+            Sauce.where('_id', sanitize(sauceId)).updateOne({ usersLiked: sanitize(usersLiked), likes: sanitize(totalLikesOrDislikes) }).then(() => {
+                resolve('Success');
+            }).catch(err => reject(err));
+        } else if (usersDisliked) {
+            Sauce.where('_id', sanitize(sauceId)).updateOne({ usersDisliked: sanitize(usersDisliked), dislikes: sanitize(totalLikesOrDislikes) }).then(() => {
+                resolve('Success');
+            }).catch(err => reject(err));
+        } else {
+            reject('Error');
+        }
+    });
 };
 module.exports.putReview = putReview;
